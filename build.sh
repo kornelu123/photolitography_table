3752e9623b2fa26ed221c218cb886eb1394583f2
kornelu <kornelu12@gmail.com>
2025-12-07 21:16:26 +0100

    Adjust build.sh and init.sh scripts

--- build.sh
+++ build.sh
@@ -3,16 +3,12 @@
 export MACHINE=am335x-evm
 
 pushd tisdk
-
-/bin/bash ./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-09.03.05.02-legacy-config.txt
-
 pushd build
 
 cp ../../meta-pholit/conf/*.conf ./conf
 
 source conf/setenv
 bitbake core-image-pgraphy -f
-
 
 popd
 popd
--- init.sh
+++ init.sh
@@ -3,4 +3,5 @@
 git submodule update --init
 pushd tisdk
 git checkout ccb3361f088963654df1a03b5db3405472b2b36c
+/bin/bash ./oe-layertool-setup.sh -f configs/processor-sdk/processor-sdk-09.03.05.02-legacy-config.txt
 popd
